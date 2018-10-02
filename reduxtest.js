115b19bc595a64253a52003d518716807ae0213f
katherinestout <kjs14@hood.edu>
2018-10-02 11:22:17 -0400

    created action ADD

--- reduxtest.js
+++ reduxtest.js
@@ -8,7 +8,15 @@ const initialState = {
 };
 
 const myReducer = (state = initialState, action) => {
+        //this is a copy of the state in a new state
+        const newState = {...state};
 
+        //if the action is ADD then add one
+        if(action.type === 'ADD'){
+            newState.age += 1;
+        }
+
+        return newState;
 }
 
 const store = createStore(myReducer);
@@ -17,6 +25,11 @@ const store = createStore(myReducer);
 this completes our reducer!!
 */
 
-//we need to change the age so we will create an action
+//we need to change the age so we will create an ACTION (a command)
+store.dispatch({type: 'ADD'});
+//this line ^ dispatches ONE action called ADD
+//dispatch 
 
+console.log(store.getState());
+//run node reduxtest.js
 
