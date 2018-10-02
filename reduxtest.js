7e07af8291f4e6783f37cfcaa4469327622a8204
katherinestout <kjs14@hood.edu>
2018-10-02 11:33:44 -0400

    fini

--- reduxtest.js
+++ reduxtest.js
@@ -13,10 +13,12 @@ const myReducer = (state = initialState, action) => {
 
         //if the action is ADD then add one
         if(action.type === 'ADD'){
-            newState.age += 1;
+            //newState.age += 1;
+            newState.age += action.val;
         }
         if(action.type === 'SUBTRACT'){
-            newState.age -= 1;
+            //newState.age -= 1;
+            newState.age += action.val;
         }
 
         return newState;
@@ -24,18 +26,25 @@ const myReducer = (state = initialState, action) => {
 
 const store = createStore(myReducer);
 
+store.subscribe(() => {
+    //this line will console log every time there is a change in state
+    console.log('initial state' + JSON.stringify(store.getState()));
+})
+
 /*const store = createStore(myReducer);
 this completes our reducer!!
 */
 
-console.log('initial state' + JSON.stringify(store.getState()));
+
 //we need to change the age so we will create an ACTION (a command)
-store.dispatch({type: 'ADD'});
+//store.dispatch({type: 'ADD'});
 //this line ^ dispatches ONE action called ADD
-//dispatch 
-console.log('after add' + JSON.stringify(store.getState()));
-store.dispatch({type:'SUBTRACT'});
-console.log('after subtract' + JSON.stringify(store.getState()));
+
+store.dispatch({type: 'ADD', val: 10});
+//add a payload of 10
+
+store.dispatch({type:'SUBTRACT', val: 5});
+
 //console.log(store.getState());
 //run node reduxtest.js
 
